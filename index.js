21f0fe310dc373bc4878601f476e33a5343ec6fd
Patrick Kim <pkim050@gmail.com>
2019-06-07 18:54:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function produceDrivingRange(range) {
+  return function(beginning, end) {
+    const distance = end - beginning;
+    const check = Math.abs(range - distance);
+    if (distance <= range) {
+      return `within range by ${check}`;
+    }
+    else if (distance > range) {
+      return `${check} blocks out of range`;
+    }
+  }
+}
+
+function produceTipCalculator(percentage) {
+  return function(total) {
+    return total * percentage;
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.id = ++driverId;
+      this.name = name;
+    }
+  }
+}
